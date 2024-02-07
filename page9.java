572c7b7892f6af950d906f75e5b454f5b789d6a9
LukasW270 <lwitek270@gmail.com>
2024-02-07 14:41:38 -0500

    something

---
+++ page9.java
@@ -0,0 +1,15 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Lukas Witek
+*Date: 2/7/24
+*Program Name: page9.java
+ ************************************/
+ public class page9{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World"); //message to user
+        System.out.println("LabOneA"); //assignment
+    }
+ }
+/* if you want to write a detailed comment, and it wraps around the screen, 
+use this block style of commenting*/
